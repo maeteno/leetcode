bb6112830075b10dfde5c630c5515e872cc87e49
Alan.Fu <ssisoo@live.cn>
2023-07-15 22:37:09 +0800

    feat(dp): min_path_sum

--- rust/dp/src/lib.rs
+++ rust/dp/src/lib.rs
@@ -2,3 +2,4 @@ pub mod fib;
 pub mod ladder;
 pub mod ladder_2;
 pub mod triangle;
+pub mod min_path_sum;
---
+++ rust/dp/src/min_path_sum.rs
@@ -0,0 +1,102 @@
+pub fn min_path_sum(grid: Vec<Vec<i32>>) -> i32 {
+    // f(row,col) = f(row,col) + min(f(row,col),f(row,col));
+
+    fn mini(a: i32, b: i32) -> i32 {
+        if a > b { b } else { a }
+    }
+
+    fn calc(grid: &Vec<Vec<i32>>, row: usize, col: usize) -> i32 {
+        if row == 0 && col == 0 {
+            return grid[0][0];
+        }
+
+        if col == 0 {
+            return grid[col][row] + calc(grid, row - 1, col);
+        }
+
+        if row == 0 {
+            return grid[col][row] + calc(grid, row, col - 1);
+        }
+
+        return grid[col][row] + mini(calc(grid, row - 1, col), calc(grid, row, col - 1));
+    }
+
+    let row = grid[0].len();
+    let col = grid.len();
+
+    calc(&grid, row - 1, col - 1)
+}
+
+pub fn min_path_sum_mem(grid: Vec<Vec<i32>>) -> i32 {
+    // f(row,col) = f(row,col) + min(f(row -1,col),f(row,col -1));
+
+    fn mini(a: i32, b: i32) -> i32 {
+        if a > b { b } else { a }
+    }
+
+    fn calc(grid: &Vec<Vec<i32>>, row: usize, col: usize, mem: &mut Vec<Vec<i32>>) -> i32 {
+        if mem[col][row] != -1 {
+            return mem[col][row];
+        }
+
+        if row == 0 && col == 0 {
+            return grid[0][0];
+        }
+
+        mem[col][row] = if col == 0 {
+            grid[col][row] + calc(grid, row - 1, col, mem)
+        } else if row == 0 {
+            grid[col][row] + calc(grid, row, col - 1, mem)
+        } else {
+            grid[col][row] + mini(calc(grid, row - 1, col, mem), calc(grid, row, col - 1, mem))
+        };
+
+        mem[col][row]
+    }
+
+    let row = grid[0].len();
+    let col = grid.len();
+
+    let mut mem = vec![vec![-1; row]; col];
+
+    calc(&grid, row - 1, col - 1, &mut mem)
+}
+
+
+// 测试
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_minimum_total() {
+        let grid = vec![
+            vec![1, 3, 1],
+            vec![1, 5, 1],
+            vec![4, 2, 1],
+        ];
+
+        assert_eq!(min_path_sum(grid), 7);
+    }
+
+    #[test]
+    fn test_minimum_total_1() {
+        let grid = vec![
+            vec![1, 1, 3],
+            vec![1, 5, 1],
+        ];
+
+        assert_eq!(min_path_sum(grid), 6);
+    }
+
+    #[test]
+    fn test_minimum_total_mem() {
+        let grid = vec![
+            vec![1, 3, 1],
+            vec![1, 5, 1],
+            vec![4, 2, 1],
+        ];
+
+        assert_eq!(min_path_sum_mem(grid), 7);
+    }
+}
