ac49d082fb18485b4fb7316016ca59881f98b56c
Alan.Fu <ssisoo@live.cn>
2023-07-15 23:18:40 +0800

    feat(dp): mini path

--- rust/dp/src/min_path_sum.rs
+++ rust/dp/src/min_path_sum.rs
@@ -62,6 +62,36 @@ pub fn min_path_sum_mem(grid: Vec<Vec<i32>>) -> i32 {
     calc(&grid, row - 1, col - 1, &mut mem)
 }
 
+// 动态规划
+pub fn min_path_sum_dp(grid: Vec<Vec<i32>>) -> i32 {
+    // f(row,col) = f(row,col) + min(f(row -1,col),f(row,col -1));
+
+    fn mini(a: i32, b: i32) -> i32 {
+        if a > b { b } else { a }
+    }
+
+    let row = grid[0].len();
+    let col = grid.len();
+
+    let mut dp = vec![vec![-1; row]; col];
+
+    for x in 0..col {
+        for y in 0..row {
+            dp[x][y] = if x == 0 && y == 0 {
+                grid[0][0]
+            } else if x == 0 {
+                grid[x][y] + dp[x][y - 1]
+            } else if y == 0 {
+                grid[x][y] + dp[x - 1][y]
+            } else {
+                grid[x][y] + mini(dp[x - 1][y], dp[x][y - 1])
+            };
+        }
+    }
+
+    dp[col - 1][row - 1]
+}
+
 
 // 测试
 #[cfg(test)]
@@ -98,5 +128,16 @@ mod tests {
         ];
 
         assert_eq!(min_path_sum_mem(grid), 7);
+    }
+
+    #[test]
+    fn test_minimum_total_dp() {
+        let grid = vec![
+            vec![1, 3, 1],
+            vec![1, 5, 1],
+            vec![4, 2, 1],
+        ];
+
+        assert_eq!(min_path_sum_dp(grid), 7);
     }
 }
